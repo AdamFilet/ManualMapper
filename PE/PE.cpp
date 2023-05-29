d64751608c6841c7b547adf91a176a3249bb6cf8
Adam <adamjaramillo0725@gmail.com>
2023-05-29 16:59:25 -0500

    Added ResolveFileOffset and ParseImports

--- PE/PE.cpp
+++ PE/PE.cpp
@@ -45,5 +45,81 @@ bool PE::Initialize(const std::string& path)
 		this->m_Sections.emplace_back(*sectionHeader);
 	}
 
+	if (!this->parseImports())
+	{
+		SPDLOG_ERROR("Failed to parse imports");
+		return false;
+	}
+
+	return true;
+}
+
+uintptr_t PE::ResolveFileOffset(uintptr_t rva)
+{
+	for (const auto& sectionHeader : m_Sections)
+	{
+		if (rva >= sectionHeader.VirtualAddress && rva < sectionHeader.VirtualAddress + sectionHeader.Misc.VirtualSize)
+		{
+			return reinterpret_cast<uintptr_t>(m_Buffer.data()) + sectionHeader.PointerToRawData + (rva - sectionHeader.VirtualAddress);
+		}
+	}
+
+	return 0;
+}
+
+bool PE::parseImports()
+{
+	auto& importData = this->m_NtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+	if (importData.Size == 0)
+	{
+		return true;
+	}
+
+	IMAGE_IMPORT_DESCRIPTOR* importDescriptor = this->ResolveFileOffset<IMAGE_IMPORT_DESCRIPTOR>(importData.VirtualAddress);
+	if (importDescriptor == nullptr)
+	{
+		return false;
+	}
+
+	for (; importDescriptor->Name; ++importDescriptor)
+	{
+		uint32_t iatIndex = 0;
+		uint32_t thunk = importDescriptor->OriginalFirstThunk ? importDescriptor->OriginalFirstThunk : importDescriptor->FirstThunk;
+		uint8_t* rvaPointer = this->ResolveFileOffset<uint8_t>(thunk);
+		std::string dllString = this->ResolveFileOffset<char>(importDescriptor->Name);
+
+		IMAGE_THUNK_DATA64* thunkData = reinterpret_cast<IMAGE_THUNK_DATA64*>(rvaPointer);
+		while (thunkData->u1.AddressOfData)
+		{
+			uintptr_t addressOfData = thunkData->u1.AddressOfData;
+			IMAGE_IMPORT_BY_NAME* addressTable = this->ResolveFileOffset<IMAGE_IMPORT_BY_NAME>(addressOfData);
+
+			Import currentImport;
+			if (addressOfData < IMAGE_ORDINAL_FLAG64 && addressTable->Name[0])
+			{
+				currentImport.Name = addressTable->Name;
+				currentImport.Ordinal = 0;
+			}
+			else
+			{
+				currentImport.Ordinal = addressOfData & 0xFFFF;
+				currentImport.Name = "None";
+			}
+
+			if (importDescriptor->FirstThunk)
+			{
+				currentImport.Pointer = importDescriptor->FirstThunk + iatIndex;
+			}
+			else
+			{
+				currentImport.Pointer = addressOfData - reinterpret_cast<uintptr_t>(this->m_Buffer.data());
+			}
+
+			thunkData++;
+			iatIndex += sizeof(uint64_t);
+			this->m_Imports[dllString].emplace_back(currentImport);
+		}
+	}
+
 	return true;
 }
--- PE/PE.hpp
+++ PE/PE.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <unordered_map>
 #include <Windows.h>
 #include <string>
 #include <vector>
@@ -7,12 +8,33 @@
 class PE
 {
 public:
+	struct Import
+	{
+		uint16_t Ordinal;
+		std::string Name;
+		uintptr_t Pointer;
+	};
+
+public:
 	bool Initialize(const std::string& path);
+
+public:
+	template<typename T>
+	T* ResolveFileOffset(uintptr_t rva)
+	{
+		return reinterpret_cast<T*>(this->ResolveFileOffset(rva));
+	}
+
+	uintptr_t ResolveFileOffset(uintptr_t rva);
+
+private:
+	bool parseImports();
 
 public:
 	uint32_t m_ImageSize;
 	IMAGE_NT_HEADERS* m_NtHeaders;
 	std::vector<IMAGE_SECTION_HEADER> m_Sections;
+	std::unordered_map<std::string, std::vector<Import>> m_Imports;
 
 public:
 	std::vector<uint8_t> m_Buffer;
