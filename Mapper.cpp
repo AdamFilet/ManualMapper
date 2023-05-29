aaec5b3a4b21b0261bde8ca9dc0061834e31080c
Adam <adamjaramillo0725@gmail.com>
2023-05-29 18:09:02 -0500

    Finished bulk of mapper now need to hijack control flow

--- Mapper.cpp
+++ Mapper.cpp
@@ -1,7 +1,9 @@
 #include "Mapper.hpp"
 
-#include "Process/Process.hpp"
 #include "PE/PE.hpp"
+#include "PE/PEHelpers.hpp"
+
+#include "Process/Process.hpp"
 
 #include <spdlog/spdlog.h>
 
@@ -27,16 +29,38 @@ bool Mapper::SelectFile(const std::string& filePath)
 
 bool Mapper::MapImage()
 {
-	uint64_t imageSize = this->m_File.m_NtHeaders->OptionalHeader.SizeOfImage;
+	uintptr_t imageSize = this->m_File.m_NtHeaders->OptionalHeader.SizeOfImage;
 	uintptr_t mappedImage = this->m_Process.AllocateMemory(imageSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
 	if (mappedImage == 0)
 	{
 		SPDLOG_ERROR("Failed to allocate memory");
-
 		return false;
 	}
 
+	if (!PEHelpers::CopySections(m_File, m_Process, mappedImage))
+	{
+		SPDLOG_INFO("Failed to copy sections");
+		return false;
+	}
 
+	if (!PEHelpers::ResolveRelocations(m_File, m_Process, mappedImage))
+	{
+		SPDLOG_INFO("Failed to resolve relocations");
+		return false;
+	}
+
+	if (!PEHelpers::ResolveImports(m_File, m_Process, mappedImage))
+	{
+		SPDLOG_INFO("Failed to resolve imports");
+		return false;
+	}
+
+	if (!PEHelpers::FixProtections(m_File, m_Process, mappedImage))
+	{
+		SPDLOG_INFO("Failed to fix protections");
+		return false;
+	}
 
 	return true;
 }
