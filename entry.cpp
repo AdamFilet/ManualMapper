f609415fe98ec6327ff9acda8f1ac0d50d2cbd07
Adam <adamjaramillo0725@gmail.com>
2023-05-31 18:52:17 -0500

    Spacing fix

--- entry.cpp
+++ entry.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-	constexpr const char* DLL_PATH = "DLL_PATH";			// Put your DLL Path here
-	constexpr const char* PROCESS_NAME = "PROCESS_NAME";	// Put the desired process to manually map into here
+	constexpr const char* DLL_PATH = "DLL_PATH"; // Put your DLL Path here
+	constexpr const char* PROCESS_NAME = "PROCESS_NAME"; // Put the desired process to manually map into here
 
 	Mapper map;
 	map.SelectFile(DLL_PATH);
