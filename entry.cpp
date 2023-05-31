13ed99b1e00bae928f4a026889133987aff9f077
Adam <adamjaramillo0725@gmail.com>
2023-05-31 18:33:53 -0500

    Example on how to use

--- entry.cpp
+++ entry.cpp
@@ -1,13 +1,16 @@
 #include <string>
 
-#include "PE/PE.hpp"
-
-#define DLLPATH "C:\\Users\\Adam\\source\\repos\\ManualMapper\\Test\\TestDLL.dll"
+#include "Mapper.hpp"
 
 int main()
 {
-	PE file;
-	file.Initalize(DLLPATH);
+	constexpr const char* DLL_PATH = "DLL_PATH";			// Put your DLL Path here
+	constexpr const char* PROCESS_NAME = "PROCESS_NAME";	// Put the desired process to manually map into here
+
+	Mapper map;
+	map.SelectFile(DLL_PATH);
+	map.SelectProcess(PROCESS_NAME);
+	map.MapImage();
 
 	return 0;
 }
